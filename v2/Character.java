a113e13da91c92cf08d283438f1110b50693f900
Your Name <you@example.com>
2017-11-10 14:04:56 -0500

    Added attributes to character

---
+++ v2/Character.java
@@ -0,0 +1,20 @@
+public class Character {
+	//Attributes
+	private static int life;
+	private static int strength;
+	private static int defense;
+	private static double aRate;
+
+
+	public static void main(String[] args) {
+
+
+	}
+
+
+
+
+
+
+
+}
