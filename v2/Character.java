e487bead9396c1d2d5ed60e4e047f5da47def6c7
ijon9 <ijon@stuy.edu>
2017-11-11 19:35:26 -0500

    Add methods to Character

--- v2/Character.java
+++ v2/Character.java
@@ -4,10 +4,35 @@ public class Character {
 	private static int strength;
 	private static int defense;
 	private static double aRate;
+	
+	//Methods
+	public static boolean isAlive() {
+		if (life == 0)
+			return false;
+		return true;
+	}
+	
+	public static int getDefense() {
+		return defense;
+	}
+	
+	public static void lowerHP(int amt) {
+		if (amt > life)
+			life = 0;
+		life -= amt;
+	}
+	
+	public static void attack(Character user) {
+		int damage;
+		if (aRate < 0)
+			damage = 0;
+		damage = (int)((strength * aRate) - user.getDefense());
+	}
+		
 
 
 	public static void main(String[] args) {
-
+		
 
 	}
 
